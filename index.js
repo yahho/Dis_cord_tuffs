88299deb47cf16809280c09c2fab23a2bf14691b
yahho <minecraftgasukinafumi@gmail.com>
2019-03-26 21:17:59 +0900

    add feature: get video channel link

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ distuff_util.pinnedmsgids=JSON.parse(FS.readFileSync("pinned.json","utf-8"));
 var pinobservechs = distuff_util.PinObserveChs;
 var pindestch = distuff_util.PinDestCh;
 
+//GetVidChLink用のストリング
+const vidlinkbase = ["https://canary.discordapp.com/channels/", "/"]
+
 //ログイン用関数
 function loginer() {
     client0.login(tokens[0]);
@@ -290,6 +293,24 @@ client0.on('message', message => {
                         message.channel.send(returnedlns)
                     }
                 }
+            } else if (message.content.indexOf('GetVidChLink') == 7) {
+                //踏むとビデオ通話の画面が開くcanaryのリンクを生成します。
+                let targetvoicech = message.member.voiceChannel
+                if (targetvoicech == null) {
+                    message.channel.send('あんたが参加してるVCが、ないやん！\nどうしてくれるの、これ。').then(sendedres => {
+                        setTimeout(sendedres.delete, 6000)
+                    })
+                }else{
+                    let res = new Discord.RichEmbed();
+                    res.setTitle('ご注文はこちらのビデオ通話ですか？');
+                    res.setColor([156, 58, 190]);
+                    res.addField("お待たせいたしました。こちら、", `[${targetvoicech[0].name}](${vidlinkbase[0]}${targetvoicech[0].guild.id}${vidlinkbase[1]}${targetvoicech[0].id})になります。`);
+                    res.setFooter("以上でよろしいですね。");
+                    message.channel.send(res).then(sendedres => {
+                        setTimeout(sendedres.delete, 6000)
+                    });
+                }
+
             }
         }
     }
