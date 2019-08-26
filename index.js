484eb48b805ca76157368b3b9c24511a96847350
yahho <minecraftgasukinafumi@gmail.com>
2019-08-27 00:55:15 +0900

    debug

--- index.js
+++ index.js
@@ -221,10 +221,11 @@ client0.on('message', message => {
                 } else if (ArrayedCmd[1].indexOf('genEmojiJSON') == 0){
                     //絵文字のJSONをEmojicord対応形式で出力する。
                     let guildemojis = message.guild.emojis.array();
-                    guildemojis.forEach(emoji =>{emoji = new distuff_util.EmojiCache(null,null,null,emoji);})
+                    console.log(guildemojis);
+                    guildemojis.forEach(emoji =>{emoji = new distuff_util.EmojiCache(null,null,null,emoji);});
                     guildemojis=new distuff_util.EmojiStorage(guildemojis);
-                    let tmpgemojis = new distuff_util.GuildEmojiStorage(message.guild.name, message.guild.id, guildemojis)
-                    let tmp = {groups:[tmpgemojis]}
+                    let tmpgemojis = new distuff_util.GuildEmojiStorage(message.guild.name, message.guild.id, guildemojis);
+                    let tmp = {groups:[tmpgemojis]};
                     FS.writeFileSync(`${message.guild.name}.json`, JSON.stringify(tmp, null,`\t`))
                     message.channel.send({files:[{attachment: `${message.guild.name}.json`,name:`${message.guild.name}.json`}]})
                 }
